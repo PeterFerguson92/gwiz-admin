bd1b8e23617b30f39a3699bd1b0b4d3d060acaa4
jrferguson <1q2w3e4r5t6y>
2025-08-01 14:49:23 +0100

    added allowed host

--- gwiz_admin/settings.py
+++ gwiz_admin/settings.py
@@ -162,3 +162,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS")
