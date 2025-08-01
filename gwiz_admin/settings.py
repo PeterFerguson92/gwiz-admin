b0456e31030641d7344d6700115bea2b86354274
jrferguson <1q2w3e4r5t6y>
2025-08-01 14:52:41 +0100

    removed duplicate property

--- gwiz_admin/settings.py
+++ gwiz_admin/settings.py
@@ -162,6 +162,3 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
-CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS")
