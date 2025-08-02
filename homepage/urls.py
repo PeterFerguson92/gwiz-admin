84e844cfb2b4119186edb291528e3f7db6f88458
jrferguson <1q2w3e4r5t6y>
2025-08-02 22:51:13 +0100

    refactored banner urls

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import BannerDetailView, BannerListView
+from .app_urls.banner_views import BannerDetailView, BannerListView
 
 urlpatterns = [
     path("banners", BannerListView.as_view()),
