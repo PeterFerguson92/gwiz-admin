03c2c33ce62b4d6f038e363c8e6dc04de0dd0cb5
jrferguson <1q2w3e4r5t6y>
2025-08-04 22:28:46 +0100

    added homepage view and serializer

--- homepage/serializers.py
+++ homepage/serializers.py
@@ -1,8 +1,14 @@
-from homepage.models import Banner
+from homepage.models import Banner, Homepage
 from rest_framework import serializers
 
 
 class BannerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Banner
+        fields = "__all__"
+
+class HomepageSerializer(serializers.ModelSerializer):
+    banner = BannerSerializer(many=True, read_only=True)
+    class Meta:
+        model = Homepage
         fields = "__all__"
--- homepage/urls.py
+++ homepage/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from homepage.app_views.banner_views import BannerDetailView, BannerListView
+from homepage.app_views.homepage_view import HomepageDetailView, HomepageListView
 
 
 
 urlpatterns = [
+    path("", HomepageListView.as_view()),
+    path("detail/<uuid:pk>", HomepageDetailView.as_view()),
     path("banners", BannerListView.as_view()),
     path("banner/detail/<uuid:pk>", BannerDetailView.as_view()),
 ]
