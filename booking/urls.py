3becdf27970a82b90f672d6abacc587b5c495e24
jrferguson <1q2w3e4r5t6y>
2025-12-01 22:08:32 +0000

    chore(booking): normalize URLs to include trailing slashes

--- booking/urls.py
+++ booking/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import (
     ActiveFitnessClassListView,
+    AllUpcomingSessionsView,
     FitnessClassListView,
     FitnessClassSessionsView,
     UpcomingClassSessionListView,
@@ -18,6 +19,11 @@ urlpatterns = [
         "fitness-classes/active",
         ActiveFitnessClassListView.as_view(),
         name="active-fitness-classes",
+    ),
+    path(
+        "sessions/all-upcoming",
+        AllUpcomingSessionsView.as_view(),
+        name="all-upcoming-sessions",
     ),
     path(
         "fitness-classes/<uuid:pk>/sessions/",
